b942d7c4837acfd276bb0b450db04eca91312bd8
IsaacPc <iamisaacolaniyi@gmail.com>
2021-09-13 16:06:06 +0100

    further tasks completed

---
+++ 100daysJsChallenge/internetStatus/static/app.js
@@ -0,0 +1,29 @@
+let statusImg = document.getElementById("statusImg");
+let statusAlert = document.getElementById("messageAlert");
+
+let connected = "./static/images/wifionn.jpg"
+let disconnected = "./static/images/noInternet.png"
+    
+
+
+window.addEventListener("load", checkConnStatus());
+
+function checkConnStatus() {
+    setInterval(checkStatus,1500);
+}
+
+function checkStatus(e) {
+    if (window.navigator.onLine) {
+        statusImg.src = connected;
+        statusAlert.innerText = "Connected!";
+        statusAlert.style.color = "blue"
+        console.log("connected");
+        
+    } else {
+        statusImg.src = disconnected;
+        statusAlert.innerText = "No internet Connection!";
+        console.log("disconnected");
+         
+    }
+}
+
